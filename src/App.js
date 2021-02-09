ad6144a768c6a7f12f3463fba00c7d278a72b413
PodiWan <tpodina@gmail.com>
2021-02-09 18:57:07 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className = 'center'>
+        <h1>CFOP.</h1>
+        <h2>Learn it, master it!</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
